606f625982a19173be13d4a0d93db01355e86eae
Jerrssey <fankton@gmail.com>
2022-12-17 16:18:28 +0200

    Task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createGallaryItemsMarkup(galleryItems)
+);
+
+galleryContainer.addEventListener("click", createModalMarkup);
+
+function createGallaryItemsMarkup(galleryItems) {
+  return [...galleryItems]
+    .map(
+      (item) => `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`
+    )
+    .join("");
+}
+
+function getUrlFromImageItem(event) {
+  event.preventDefault();
+  const { nodeName, dataset } = event.target;
+  if (nodeName !== "IMG") {
+    return;
+  }
+  return dataset.source;
+}
+
+function createModalMarkup(event) {
+    if (!getUrlFromImageItem(event)) {
+        return;
+      }
+
+  const instance = basicLightbox.create(
+    `
+  <img src="${getUrlFromImageItem(event)}" alt="${event.target.alt}">
+`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", (event) => {
+          if (event.code === "Escape") {
+            instance.close();
+          }
+        });
+      },
+    }
+  );
+
+  instance.show();
+}
