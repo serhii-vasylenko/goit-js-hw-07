8c9f4b277f8519eba977230a9efe235a1f4d8498
Jerrssey <fankton@gmail.com>
2022-12-18 16:03:57 +0200

    Task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createGallaryItemsMarkup(galleryItems)
+);
+
+galleryContainer.addEventListener("click", imageClickedCheck);
+
+function createGallaryItemsMarkup(galleryItems) {
+  return [...galleryItems]
+    .map(
+      (item) => `
+      <li>
+        <a class="gallery__item" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+        </a>
+    </li>
+    `
+    )
+    .join("");
+}
+
+function imageClickedCheck(event) {
+  event.preventDefault();
+  const { nodeName } = event.target;
+  if (nodeName !== "IMG") {
+    console.log("Clicked element is not an image!");
+    return;
+  }
+
+  showGallery();
+}
+
+function showGallery() {
+  const gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    overlayOpacity: 0.5,
+    showCounter: false,
+  });
+
+  gallery.open();
+
+  gallery.on("closed.simplelightbox", () => {
+    gallery.refresh();
+    galleryContainer.removeEventListener("click", imageClickedCheck);
+  });
+}
